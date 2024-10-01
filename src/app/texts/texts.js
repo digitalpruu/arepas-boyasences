6f16a18811122d14a290cbc273bbab22224b8921
kevinpicon01 <digitalpruu@gmail.com>
2024-09-30 22:58:42 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,9 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Arepas Boyasences";
+const email = "GRECIAGUTIERREZ1991@HOTMAIL.COM";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    `¡Hola! Estoy interesado en las deliciosas arepas boyacenses de ${title} y me gustaría recibir más información. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "+573209882629";
 const textos = {
     index: {
         title: title,
@@ -12,25 +12,24 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "En Arepas Boyasences, nos dedicamos a la elaboración artesanal de las auténticas arepas boyacenses, una tradición culinaria que trae el sabor de Boyacá a tu mesa. Cada arepa está hecha con ingredientes de alta calidad, manteniendo el sabor original y la frescura que caracteriza a este delicioso manjar.",
+        description2: "Nuestro compromiso es brindarte la mejor experiencia gastronómica, con productos frescos y artesanales que te transportan a las montañas de Boyacá en cada bocado. Ya sea para tus reuniones familiares, eventos o simplemente para disfrutar en casa, nuestras arepas boyacenses son la opción perfecta.",
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
         email: email
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Arepas boyacenses auténticas y deliciosas",
+            p2: "El sabor de Boyacá en cada bocado",
         },
-        address: "Dirección del emprendimiento",
+        address: "",
+        instagram: "https://www.instagram.com/",
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+        facebook: "https://www.facebook.com/gracia.gutierrez",
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
     },
-
 };
 export default textos;
